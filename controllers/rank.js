afc636fe85afff9535fcd9cda68d25bb30bc2952
Mike Shohet <mshohet@Mikes-MacBook-Pro.local>
2020-07-22 15:32:24 -0400

    Commiting all project files

---
+++ controllers/rank.js
@@ -0,0 +1,30 @@
+const clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'facdef33794149eb87a593f06a9eed71'
+});
+
+const handleAPICall = (req, res) => {
+	app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+    	res.json(data);
+    })
+    .catch(err => res.status(400).json("Unable to connect with API"));
+}
+
+const handleRank = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+		.increment('entries', 1)
+		.returning('entries')
+		.then(entries => {
+			res.json(entries[0]);
+		})
+		.catch(err => res.status(400).json('Unable to get count.'));
+}
+
+module.exports = {
+	handleRank,
+	handleAPICall
+};
---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const bcrypt = require('bcrypt-nodejs');
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const rank = require('./controllers/rank');
+
+const db = knex ({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'mshohet',
+    password : '',
+    database : 'facerec'
+  }
+});
+
+db.select('*').from('users')
+	.then(data => {
+		console.log(data);
+	}); //KNEX created SQL command
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send(database.users);
+});
+
+// SIGN IN
+
+app.post('/signin', (req, res) => {
+	signin.handleSignin(req, res, db, bcrypt);
+});
+
+// REGISTER
+
+app.post('/register', (req, res) => { 
+	register.handleRegister(req, res, db, bcrypt)
+});
+
+// PROFILE
+
+app.get('/profile/:id', (req, res) => {
+	profile.handleProfileGet(req, res, db)
+});
+
+// RANK
+
+app.put('/rank', (req, res) => {
+	rank.handleRank(req, res, db)
+});
+
+app.listen(3000, () => {
+	console.log('server is up on port 3000');
+});
